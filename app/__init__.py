2f5afccc5419bf13e2d5634ebdcbfb4a2ce82ae8
Anastasia Dorofeeva <dorofeevanastasia21@gmail.com>
2024-12-01 06:35:37 +0300

    add init

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from prefix_tree import PrefixTreeNode, PrefixTree
+from word_completor import WordCompletor
+from ngram_model import NGramLanguageModel
+from text_suggestion import TextSuggestion
+
